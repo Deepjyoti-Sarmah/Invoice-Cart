b8ad90243626390860af640dfd9a249ac05e8d9d
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-02-21 16:30:22 +0530

    Adding conform server and client validation

--- components/CreateInvoice.tsx
+++ components/CreateInvoice.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { useActionState, useState } from 'react'
 import { Card, CardContent } from './ui/card'
 import { Badge } from './ui/badge'
 import { Input } from './ui/input'
@@ -13,169 +13,253 @@ import { PopoverContent } from './ui/popover'
 import { Calendar } from './ui/calendar'
 import { Textarea } from './ui/textarea'
 import SubmitButton from './SubmitButton'
+import { createInvoice } from '@/app/actions'
+import { useForm } from '@conform-to/react'
+import { parseWithZod } from '@conform-to/zod'
+import { invoiceSchema } from '@/app/utils/zodSchemas'
 
 export default function CreateInvoice() {
+  const [lastResult, action] = useActionState(createInvoice, undefined)
+  const [form, fields] = useForm({
+    lastResult,
+
+    onValidate({ formData }) {
+      return parseWithZod(formData, {
+        schema: invoiceSchema
+      });
+    },
+
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
+
   const [selectedDate, setSelectedDate] = useState(new Date())
 
   return (
     <Card className='w-full max-w-4xl mx-auto'>
       <CardContent className='p-6'>
-        <div className='flex flex-col gap-1 w-fit mb-6'>
-          <div className='flex items-center gap-4'>
-            <Badge variant="secondary">Draft</Badge>
-            <Input placeholder='text 123' />
-          </div>
-        </div>
-
-        <div className='grid md:grid-cols-3 gap-6 mb-6'>
-          <div>
-            <Label>Invoice No.</Label>
-            <div className='flex'>
-              <span className='px-3 border border-r-0 rounded-l-md bg-muted flex items-center'>#</span>
-              <Input className="rounded-l-none" placeholder='5' />
+        <form id={form.id} action={action} onSubmit={form.onSubmit}>
+          <div className='flex flex-col gap-1 w-fit mb-6'>
+            <div className='flex items-center gap-4'>
+              <Badge variant="secondary">Draft</Badge>
+              <Input
+                name={fields.invoiceName.name}
+                key={fields.invoiceName.key}
+                defaultValue={fields.invoiceName.initialValue}
+                placeholder='text 123' />
             </div>
+            <p className='text-sm text-red-500'>{fields.invoiceName.errors}</p>
           </div>
 
-          <div>
-            <Label>Currency</Label>
-            <Select defaultValue='USD'>
-              <SelectTrigger>
-                <SelectValue placeholder="Select Currency" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value='USD'>
-                  United States Dollar -- USD
-                </SelectItem>
-                <SelectItem value='EUR'>
-                  Euro -- EUR
-                </SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-        </div>
-
-        <div className='grid md:grid-cols-2 gap-6 mb-6'>
-          <div>
-            <Label>From</Label>
-            <div className='space-y-2'>
-              <Input placeholder='Your Name' />
-              <Input placeholder='Your Email' />
-              <Input placeholder='Your Address' />
-            </div>
-          </div>
-
-          <div>
-            <Label>To</Label>
-            <div className='space-y-2'>
-              <Input placeholder='Client Name' />
-              <Input placeholder='Client Email' />
-              <Input placeholder='Client Address' />
-            </div>
-          </div>
-        </div>
-
-        <div className='grid md:grid-cols-2 gap-6 mb-6'>
-          <div>
+          <div className='grid md:grid-cols-3 gap-6 mb-6'>
             <div>
-              <Label>Date</Label>
+              <Label>Invoice No.</Label>
+              <div className='flex'>
+                <span className='px-3 border border-r-0 rounded-l-md bg-muted flex items-center'>#</span>
+                <Input
+                  name={fields.invoiceNumber.name}
+                  key={fields.invoiceNumber.key}
+                  defaultValue={fields.invoiceNumber.initialValue}
+                  className="rounded-l-none"
+                  placeholder='5' />
+              </div>
+              <p className='text-sm text-red-500'>{fields.invoiceNumber.errors}</p>
             </div>
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button variant="outline" className='w-[280px] text-left'>
-                  <CalendarIcon />
-                  {selectedDate ? (
-                    new Intl.DateTimeFormat("en-IN", {
-                      dateStyle: "long"
-                    }).format(selectedDate)
-                  ) : (
-                    <span>Pick a date</span>
-                  )}
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent>
-                <Calendar
-                  mode='single'
-                  selected={selectedDate}
-                  onSelect={(date) => setSelectedDate(date || new Date())}
-                  fromDate={new Date()}
+
+            <div>
+              <Label>Currency</Label>
+              <Select
+                defaultValue='USD'
+                name={fields.currency.name}
+                key={fields.currency.key}
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Select Currency" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value='USD'>
+                    United States Dollar -- USD
+                  </SelectItem>
+                  <SelectItem value='EUR'>
+                    Euro -- EUR
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+              <p className='text-sm text-red-500'>{fields.currency.errors}</p>
+            </div>
+          </div>
+
+          <div className='grid md:grid-cols-2 gap-6 mb-6'>
+            <div>
+              <Label>From</Label>
+              <div className='space-y-2'>
+                <Input
+                  name={fields.fromName.name}
+                  key={fields.fromName.key}
+                  placeholder='Your Name'
                 />
-              </PopoverContent>
-            </Popover>
+                <p className='text-red-500 text-sm'>{fields.fromName.errors}</p>
+                <Input
+                  name={fields.fromEmail.name}
+                  key={fields.fromEmail.key}
+                  placeholder='Your Email'
+                />
+                <p className='text-red-500 text-sm'>{fields.fromEmail.errors}</p>
+
+                <Input
+                  name={fields.fromAddress.name}
+                  key={fields.fromAddress.key}
+                  placeholder='Your Address'
+                />
+                <p className='text-red-500 text-sm'>{fields.fromAddress.errors}</p>
+
+              </div>
+            </div>
+
+            <div>
+              <Label>To</Label>
+              <div className='space-y-2'>
+                <Input
+                  name={fields.clientName.name}
+                  key={fields.clientName.key}
+                  defaultValue={fields.clientName.initialValue}
+                  placeholder='Client Name' />
+                <p className='text-red-500 text-sm'>{fields.clientName.errors}</p>
+
+                <Input
+                  name={fields.clientEmail.name}
+                  key={fields.clientEmail.key}
+                  defaultValue={fields.clientEmail.initialValue}
+                  placeholder='Client Email' />
+                <p className='text-red-500 text-sm'>{fields.clientEmail.errors}</p>
+                <Input
+                  name={fields.clientAddress.name}
+                  key={fields.clientAddress.key}
+                  defaultValue={fields.clientAddress.initialValue}
+                  placeholder='Client Address' />
+                <p className='text-red-500 text-sm'>{fields.clientAddress.errors}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='grid md:grid-cols-2 gap-6 mb-6'>
+            <div>
+              <div>
+                <Label>Date</Label>
+              </div>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant="outline"
+                    className='w-[280px] text-left'>
+                    <CalendarIcon />
+                    {selectedDate ? (
+                      new Intl.DateTimeFormat("en-IN", {
+                        dateStyle: "long"
+                      }).format(selectedDate)
+                    ) : (
+                      <span>Pick a date</span>
+                    )}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                  <Calendar
+                    mode='single'
+                    selected={selectedDate}
+                    onSelect={(date) => setSelectedDate(date || new Date())}
+                    fromDate={new Date()}
+                  />
+                </PopoverContent>
+              </Popover>
+              <p className='text-red-500 text-sm'>{fields.date.errors}</p>
+            </div>
+
+            <div>
+              <Label>Invoice Due</Label>
+              <Select
+                name={fields.dueDate.name}
+                key={fields.dueDate.key}
+                defaultValue={fields.dueDate.initialValue}
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Select due date" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value='0'>Due on Reciept</SelectItem>
+                  <SelectItem value='15'>Net 15</SelectItem>
+                  <SelectItem value='30'>Net 30</SelectItem>
+                </SelectContent>
+              </Select>
+              <p className='text-red-500 text-sm'>{fields.dueDate.errors}</p>
+            </div>
           </div>
 
           <div>
-            <Label>Invoice Due</Label>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="Select due date" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value='0'>Due on Reciept</SelectItem>
-                <SelectItem value='15'>Net 15</SelectItem>
-                <SelectItem value='30'>Net 30</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-        </div>
-
-        <div>
-          <div className="grid grid-cols-12 gap-4 mb-2 font-medium">
-            <p className="col-span-6">Description</p>
-            <p className="col-span-2">Quantity</p>
-            <p className="col-span-2">Rate</p>
-            <p className="col-span-2">Amount</p>
-          </div>
+            <div className="grid grid-cols-12 gap-4 mb-2 font-medium">
+              <p className="col-span-6">Description</p>
+              <p className="col-span-2">Quantity</p>
+              <p className="col-span-2">Rate</p>
+              <p className="col-span-2">Amount</p>
+            </div>
 
 
-          <div className='grid grid-cols-12 gap-4 mb-6'>
-            <div className='col-span-6'>
-              <Textarea
-                placeholder='Item name & description'
-              />
-            </div>
-            <div className='col-span-2'>
-              <Input
-                type='number'
-                placeholder='0'
-              />
-            </div>
-            <div className='col-span-2'>
-              <Input
-                type='number'
-                placeholder='0'
-              />
-            </div>
-            <div className='col-span-2'>
-              <Input
-                disabled
-              />
+            <div className='grid grid-cols-12 gap-4 mb-6'>
+              <div className='col-span-6'>
+                <Textarea
+                  name={fields.invoiceItemDescription.name}
+                  key={fields.invoiceItemDescription.key}
+                  defaultValue={fields.invoiceItemDescription.initialValue}
+                  placeholder='Item name & description'
+                />
+                <p className='text-red-500 text-sm'>{fields.invoiceItemDescription.errors}</p>
+              </div>
+              <div className='col-span-2'>
+                <Input
+                  name={fields.invoiceItemQuantity.name}
+                  key={fields.invoiceItemQuantity.key}
+                  type='number'
+                  placeholder='0'
+                />
+                <p className='text-red-500 text-sm'>{fields.invoiceItemQuantity.errors}</p>
+              </div>
+              <div className='col-span-2'>
+                <Input
+                  type='number'
+                  placeholder='0'
+                />
+              </div>
+              <div className='col-span-2'>
+                <Input
+                  disabled
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className='flex justify-end'>
-          <div className='w-1/3'>
-            <div className='flex justify-between py-2'>
-              <span>Subtotal</span>
-              <span>$5.00</span>
-            </div>
-            <div className='flex justify-between py-2 border-t'>
-              <span>Total (USD)</span>
-              <span className='font-medium underline underline-offset-2'>$5.00</span>
+          <div className='flex justify-end'>
+            <div className='w-1/3'>
+              <div className='flex justify-between py-2'>
+                <span>Subtotal</span>
+                <span>$5.00</span>
+              </div>
+              <div className='flex justify-between py-2 border-t'>
+                <span>Total (USD)</span>
+                <span className='font-medium underline underline-offset-2'>$5.00</span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div>
-          <Label>Note</Label>
-          <Textarea placeholder='Add your Note/s right here...' />
-        </div>
-
-        <div className='flex items-center justify-end mt-6'>
           <div>
-            <SubmitButton text='Send Invoice to Client' />
+            <Label>Note</Label>
+            <Textarea placeholder='Add your Note/s right here...' />
           </div>
-        </div>
+
+          <div className='flex items-center justify-end mt-6'>
+            <div>
+              <SubmitButton text='Send Invoice to Client' />
+            </div>
+          </div>
+        </form>
       </CardContent>
     </Card>
   )
