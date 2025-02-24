32196af1cc4230c2464a85412c7fa2733fcc4105
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-02-24 21:51:31 +0530

    adding cards in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import { requiredUser } from '../utils/hooks'
 import { signOut } from '../utils/auth'
+import DashboardBlocks from '@/components/DashboardBlocks'
 
 export default async function Dashboard() {
   const session = await requiredUser()
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <form
-        action={async () => {
-          "use server"
-          await signOut()
-        }}
-      >
-        <button type="submit">Sign Out</button>
-      </form>
-    </div>
+    <>
+      <DashboardBlocks />
+    </>
+
   )
 }
 
