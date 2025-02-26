cfdd73bbd9c6ca630c61d54398706e9e79f3e3b0
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-02-26 16:03:52 +0530

    added a navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/logo.png"
+import { buttonVariants } from "./ui/button";
+
+export default function Navbar() {
+  return (
+    <div className="flex items-center justify-between py-5">
+      <Link href="/" className="flex items-center gap-2">
+        <Image src={Logo} alt="logo" className="size-10" />
+        <h3 className="text-3xl font-semibold">
+          Invoice <span className="text-blue-500">Marshal</span>
+        </h3>
+      </Link>
+      <Link href="/login"
+        className={buttonVariants()}
+      >
+        Get started
+      </Link>
+    </div>
+  )
+}
+
