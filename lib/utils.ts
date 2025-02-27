405ce69cea6ea6f47afd6549e2733e4946f20a9c
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-02-27 17:05:33 +0530

    adding get base url for dynamic url

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
 import { clsx, type ClassValue } from "clsx"
+import { headers } from "next/headers"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function getBaseUrl() {
+  if (process.env.NEXT_PUBLIC_BASE_URL) {
+    return process.env.NEXT_PUBLIC_BASE_URL.trim().replace(/\/$/, '')
+  }
+
+  const headersList = await headers()
+  const host = headersList.get("host") || "localhost:3000"
+  const protocol = process.env.NODE_ENV === "production" ? "https" : "http"
+
+  return `${protocol}://${host}`
 }
