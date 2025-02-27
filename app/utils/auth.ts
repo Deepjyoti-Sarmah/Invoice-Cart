fd91b77b4bbae94b3de3d0d818176294be585be6
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-02-27 14:15:25 +0530

    adding google and github oauth login

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -2,10 +2,14 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
 import Nodemailer from "next-auth/providers/nodemailer"
 import { prisma } from "./db"
+import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
+    GitHub,
+    Google,
     Nodemailer({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
