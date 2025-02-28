4251cf7e544f33423665c30f8224a9ce32644ffe
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-02-28 12:24:29 +0530

    fixing action email sending end editing error

--- app/actions.ts
+++ app/actions.ts
@@ -7,9 +7,9 @@ import { prisma } from "./utils/db"
 import { redirect } from "next/navigation"
 import { emailClient } from "./utils/mailtrap"
 import { formatCurrency } from "./utils/formatCurrency"
-import { getBaseUrl } from "./utils/getUrl"
+// import { getBaseUrl } from "./utils/getUrl"
 
-const BASE_URL = await getBaseUrl();
+// const BASE_URL = await getBaseUrl();
 
 export async function onboardUser(prevState: any, formData: FormData) {
   const session = await requiredUser()
@@ -89,7 +89,10 @@ export async function createInvoice(prevState: any, formData: FormData) {
         amount: submission.value.total,
         currency: submission.value.currency as any,
       }),
-      "invoiceLink": `${BASE_URL}/api/invoice/${data.id}`
+      // "invoiceLink": `${BASE_URL}/api/invoice/${data.id}`
+      "invoiceLink": process.env.NODE_ENV !== "production"
+        ? `http://localhost:3000/api/invoice/${data.id}`
+        : `https://invoice-cart.vercel.app/api/invoice/${data.id}`
     }
   });
 
@@ -153,7 +156,10 @@ export async function editInvoice(prevState: any, formData: FormData) {
         amount: submission.value.total,
         currency: submission.value.currency as any,
       }),
-      "invoiceLink": `${BASE_URL}/api/invoice/${data.id}`
+      // "invoiceLink": `${BASE_URL}/api/invoice/${data.id}`
+      "invoiceLink": process.env.NODE_ENV !== "production"
+        ? `http://localhost:3000/api/invoice/${data.id}`
+        : `https://invoice-cart.vercel.app/api/invoice/${data.id}`
     }
   });
 
