42ede1948e78628d41b39dd1438cfef35042390a
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-02-28 15:51:29 +0530

    refactoring email edit, create and resend

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "@/public/logo.png"
-import { buttonVariants } from "./ui/button";
 import { RainbowButton } from "./magicui/rainbow-button";
 
 export default function Navbar() {
@@ -10,7 +9,7 @@ export default function Navbar() {
       <Link href="/" className="flex items-center gap-2">
         <Image src={Logo} alt="logo" className="size-10" />
         <h3 className="text-3xl font-semibold">
-          Invoice <span className="text-blue-500">Marshal</span>
+          Invoice<span className="text-blue-500">Cart</span>
         </h3>
       </Link>
       <Link href="/login"
